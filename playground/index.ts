1d58c28cbf5fabdcc5161106aff2a50e864c681e
joaorulff <jlrulff@gmail.com>
2023-10-18 21:10:10 -0400

    updating gitignore

--- playground/index.ts
+++ playground/index.ts
@@ -1,41 +1,39 @@
-// // import { TemporalViewer } from '../src/TemporalViewer';
+import { ModelViewer } from '../src/chart/ModelViewer.model'
 
-// import { ModelViewer } from '../src/chart/ModelViewer.model'
+const main = async () => {
 
-// const main = async () => {
+    const mainDiv: HTMLDivElement = <HTMLDivElement>document.getElementById('main-div');
+    const mv = new ModelViewer( mainDiv );
 
-//     const mainDiv: HTMLDivElement = <HTMLDivElement>document.getElementById('main-div');
-//     const mv = new ModelViewer( mainDiv );
+    // synthetic data
+    const data =  
+    { name: 'actions', 
+        labels: [
+            { name: 'tortilla', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
+            { name: 'knife', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
+            { name: 'plate', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
+            { name: 'paper towel', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
+            { name: 'board', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
+            { name: 'toothpicks', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
+            { name: 'dental floss', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
+            { name: 'jam', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() }
+    ]}
 
-//     // synthetic data
-//     const data =  
-//     { name: 'actions', 
-//         labels: [
-//             { name: 'tortilla', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
-//             { name: 'knife', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
-//             { name: 'plate', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
-//             { name: 'paper towel', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
-//             { name: 'board', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
-//             { name: 'toothpicks', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
-//             { name: 'dental floss', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() },
-//             { name: 'jam', values: Array.from({length: 200}, () => (Math.random())), confidence: Math.random(), coverage: Math.random() }
-//     ]}
-
-//     let index: number | null = null;
-//     setInterval( () => {
+    let index: number | null = null;
+    setInterval( () => {
                 
-//         mv.update( data, index, [0, 180000] );
+        mv.update( data, index, [0, 180000] );
 
-//         if( index === null ){
-//             index = 0;
-//         } else {
-//             index ++;
-//         }
+        if( index === null ){
+            index = 0;
+        } else {
+            index ++;
+        }
 
-//     }, 200)
+    }, 200)
     
     
 
-// }
+}
 
-// main();
+main();
